d82d20e186485dc88af1e32371afab4631efb788
Aurora1610 <auroralroy2003@gmail.com>
2024-04-21 20:04:23 +0600

    active route, useNavigation,  loading, useLocation.

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
   return (
     <div>
       <h1>Navbar</h1>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About us</Link>
-        <Link to="/users">Users</Link>
-        <Link to="/posts">Posts</Link>
-        <Link to="/contact">Contact us</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About us</NavLink>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
+        <NavLink to="/contact">Contact us</NavLink>
       </nav>
     </div>
   );
